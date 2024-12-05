ddc45bc279c875cc34cf49197de61c3e25c155ee
inwoo0808 <inwoo0808@gmail.com>
2024-12-05 21:12:14 +0900

    my fragment 기능 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,7 @@ dependencies {
     implementation("com.google.firebase:firebase-auth-ktx")
     implementation("com.google.firebase:firebase-storage-ktx:21.0.1")
 
+
     // 통신
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
