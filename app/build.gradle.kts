77243377083ec470aed3dccebf18a13dc4fbf73f
joohee <joo9837@naver.com>
2024-12-06 15:58:23 +0900

    feat: StatusActivity 구현

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,7 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.firebase:firebase-firestore:25.1.1")
     implementation(libs.androidx.activity)
+    implementation(libs.firebase.database.ktx)
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
